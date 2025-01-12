137f4c7cfca3a925337e25270d95493155d7db94
Tran Quang <133882661+tranquang2012@users.noreply.github.com>
2025-01-12 21:14:43 +0700

    mapsactivity to maaster (#10)
    * mapsActivity
    
    * update maps activity with more features
    
    * change hotel availability type to number
    
    ---------
    
    Co-authored-by: minhquan <s3938194@rmit.edu.vn>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
@@ -30,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -38,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
